d9ec2af323e83b0735039dcf9f5997cdc6001c7f
YYRick <2979434806@qq.com>
2021-10-04 21:20:15 +0800

    axios

--- learn_axios/src/metwork/request.js
+++ learn_axios/src/metwork/request.js
@@ -66,6 +66,34 @@ export function request(config) {
         baseURL: "http://152.136.185.210:7878/api/hy66",
         timeout: 5000
     })
-    // 2.发送真正的网络请求
+    
+    //2.axios的拦截器
+    instance.interceptors.request.use(config => {
+       console.log(config);
+       /* 
+         1.请求拦截：我们在这会做的事情
+         //1.比如config中的一些信息不符合服务器的要求
+
+         //比如请求图标(转圆圈)
+ 
+
+         //比如网络请求登录(token),必须要携带一些特殊的信息
+
+       */
+       return config
+    },err =>{
+    //    console.log(err);
+    })
+
+
+    //2.2 响应拦截
+    instance.interceptors.response.use(res =>{
+        // console.log(res);
+        return res.data
+    }, err => {
+        console.log(err);
+    })
+
+    // 3.发送真正的网络请求
     return instance(config) 
 }
